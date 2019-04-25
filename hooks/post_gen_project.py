5679b83d16772f4cb395bdf4f3eab059ee34dbf6
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-04-24 21:18:19 -0500

    Remove empty _root folder when done

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,3 +5,4 @@ for file_name in os.listdir("_root"):
     os.rename(f"_root/{file_name}", f"../{file_name}")
 
 urllib.request.urlretrieve("https://www.gitignore.io/api/django", "../.gitignore")
+os.rmdir("_root")
