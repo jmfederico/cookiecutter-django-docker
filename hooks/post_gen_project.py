c0b7ebb7c829f1ed1d4279f63d37324f7911db4a
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-07-16 20:14:09 -0500

    Code comments

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,5 @@
+"""Actions to be run after a successful Cookie bake."""
 import os
-import urllib.request
 
 for file_name in os.listdir("_root"):
     os.rename(f"_root/{file_name}", f"../{file_name}")
